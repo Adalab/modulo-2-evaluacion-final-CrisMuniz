07e5ddec4039cb26e7ecd95cd418eb8aa67d95fb
Cristina Muñiz Marín <cristinamuniz85@gmail.com>
2023-06-10 18:32:15 +0200

    add html,css and first function

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('>> Ready :)');
+const ulElement = document.querySelector('.js_character_list');
+const url = 'https://api.disneyapi.dev/character?pageSize=50';
+
+
+
+fetch(url)
+.then((response) => response.json())
+.then((inf) => {
+    console.log(inf);
+    ulElement.innerHTML = renderCharacter(inf.data[0]);
+});
+
+function renderCharacter(inf) {
+     let html =
+    ` <li><img src="${inf.imageUrl}"alt="">
+    <p>${inf.name}</p></li>`
+return html
+};
+
