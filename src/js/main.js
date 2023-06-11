1c3c3bab0614428f1ebfd85f47fe281b9602f327
Cristina Muñiz Marín <cristinamuniz85@gmail.com>
2023-06-11 11:54:33 +0200

    add favorite function

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,45 @@ const ulElement = document.querySelector('.js_character_list');
 const url = 'https://api.disneyapi.dev/character?pageSize=50';
 
 
+let characterFavorite = [];
+let characterApi = [];
 
 fetch(url)
 .then((response) => response.json())
 .then((inf) => {
-    console.log(inf);
-    ulElement.innerHTML = renderCharacter(inf.data[0]);
+    characterApi = inf.data;
+    ulElement.innerHTML = renderCharacter(characterApi);
+    generateEvents();
 });
 
-function renderCharacter(inf) {
-     let html =
-    ` <li><img src="${inf.imageUrl}"alt="">
-    <p>${inf.name}</p></li>`
-return html
+function renderCharacter(list) {
+    let html = '';
+    for(const personaje of list){
+        if( personaje.imageUrl === ''){
+            html +=`<li class="js_li_character" id="${personaje._id}"><img src="/assets/images/disney.png" alt=""><p>${personaje.name}</p></li>`
+        }
+        else {
+            html +=`<li class="js_li_character" id="${personaje._id}"><img src="${personaje.imageUrl}" alt=""><p>${personaje.name}</p></li>`
+        }
+    }
+   
+  
+    return html
 };
+
+function generateEvents(){
+    const liCharacter = document.querySelectorAll('.js_li_character');
+    for(const li of liCharacter){
+        li.addEventListener('click', handleClick);
+        console.log(li)
+        }
+        
+}
+
+function handleClick(event) {
+    const idCharacter = event.currentTarget.id;
+    console.log(idCharacter);
+}
+
+
 
